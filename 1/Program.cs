005b3961db7e7137e764d3ebb92f75ebf5ffa4d9
SergeyZSV <sergey06062001@gmail.com>
2022-04-18 17:42:42 +0300

    Task 1

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1. На вход подаются два числа n и m, такие, что n<m. Заполните массив случайными числами из промежутка [n, m].
+
+int n = 1;
+int m = 10;
+int[] array = new int[m - n];
+
+FillArray();
+
+for (int i = 0; i < array.Length; i++)
+{
+   Console.Write(array[i] + " ");
+}
+
+void FillArray()
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(n, m + 1);        
+    }
+}
